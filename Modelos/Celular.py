dd2e5742b6f9b10465cc537b005ae97fbed74f04
SNOWFORSE <134400943+SNOWFORSE@users.noreply.github.com>
2024-09-13 07:46:12 -0300

    Add files via upload

--- Modelos/Celular.py
+++ Modelos/Celular.py
@@ -1,18 +1,22 @@
 class Celular:
+    celulares = []
+
     def __init__(self, nome, modelo, estado):
         self.nome = nome
         self.modelo = modelo
         self.estado = estado
         self.ativo = False
+        Celular.celulares.append(self)
 
     def __str__(self):
         return f'{self.nome} | {self.modelo} | {self.estado} | {self.ativo}'
-celular_iPhone = Celular('iPhone','14 Pro','Novo' )
+    
+    def listar_celulares():
+        for celular in Celular.celulares:
+            print(f'{celular.nome} | {celular.modelo} | {celular.estado} | {celular.ativo}')
+
+celular_iPhone = Celular('iPhone','14 Pro','Novo')
 celular_Xiaomi = Celular('Redmi', 'Note 13 Pro+', 'Novo')
 celular_Samsung = Celular('Samsung Galaxy', 'A55', 'Novo')
 
-Celulares = [celular_iPhone, celular_Xiaomi, celular_Samsung]
-
-print(celular_iPhone)
-print(celular_Samsung)
-print(celular_Xiaomi)
+Celular.listar_celulares()
