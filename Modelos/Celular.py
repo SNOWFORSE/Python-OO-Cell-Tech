829e09d7bc615fe7a55a439b339b10c1a7affe37
SNOWFORSE <134400943+SNOWFORSE@users.noreply.github.com>
2024-09-13 10:39:43 -0300

    Add files via upload

--- Modelos/Celular.py
+++ Modelos/Celular.py
@@ -5,15 +5,27 @@ class Celular:
         self.nome = nome
         self.modelo = modelo
         self.estado = estado
-        self.ativo = False
+        self._ativo = False
         Celular.celulares.append(self)
 
     def __str__(self):
         return f'{self.nome} | {self.modelo} | {self.estado} | {self.ativo}'
     
     def listar_celulares():
+        print(''' 
+▒█▀▀█ █▀▀ █░░ █░░ 　 ▀▀█▀▀ █▀▀ █▀▀ █░░█ 　 ▒█▀▀▀ █░░ █▀▀ ▀▀█▀▀ █▀▀█ █▀▀█ █▀▀▄ ░▀░ █▀▀ █▀▀█ █▀▀ 
+▒█░░░ █▀▀ █░░ █░░ 　 ░▒█░░ █▀▀ █░░ █▀▀█ 　 ▒█▀▀▀ █░░ █▀▀ ░░█░░ █▄▄▀ █░░█ █░░█ ▀█▀ █░░ █░░█ ▀▀█ 
+▒█▄▄█ ▀▀▀ ▀▀▀ ▀▀▀ 　 ░▒█░░ ▀▀▀ ▀▀▀ ▀░░▀ 　 ▒█▄▄▄ ▀▀▀ ▀▀▀ ░░▀░░ ▀░▀▀ ▀▀▀▀ ▀░░▀ ▀▀▀ ▀▀▀ ▀▀▀▀ ▀▀▀
+''')
+        print(f'{"Nome do Celular".ljust(18)} | {"Modelo do Celular".ljust(18)} | {"Estado do Celular".ljust(18)} | {"Ativo"}')
+        print('--------------------------------------------------------------------------------------------')
         for celular in Celular.celulares:
-            print(f'{celular.nome} | {celular.modelo} | {celular.estado} | {celular.ativo}')
+            print(f'{celular.nome.ljust(18)} | {celular.modelo.ljust(18)} | {celular.estado.ljust(18)} | {celular.ativo}')
+        print('--------------------------------------------------------------------------------------------')
+
+    @property
+    def ativo(self):
+        return '☑ O celular está ativo' if self._ativo else '☒ O celular não está ativado'
 
 celular_iPhone = Celular('iPhone','14 Pro','Novo')
 celular_Xiaomi = Celular('Redmi', 'Note 13 Pro+', 'Novo')
